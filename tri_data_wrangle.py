28205e51fd49dbebba24e6c90cb28aea97c3754d
Eric Kofman <ericrkofman@gmail.com>
2015-08-16 22:37:20 -0700

    Generate processed csv file

--- tri_data_wrangle.py
+++ tri_data_wrangle.py
@@ -22,22 +22,42 @@ def convert_time_to_seconds(readout):
 	total_seconds = seconds + 60*minutes + 3600*hours
 	return total_seconds
 
+def parse_triathlon_data(filename):
+	participants = []			
+	with open(filename, 'r') as results:
+		lines = results.readlines()
+		headers = lines[0].split(',')
 
-with open('olympic_tri_results.csv', 'r') as results:
-	lines = results.readlines()
-	headers = lines[0].split(',')
+		for line in lines[1:]:
+			participant = {}
+			for i,element in enumerate(line.split(',')):
+				header = headers[i]
+				if header in ['Pos','Category', 'Cat Pos', 'Age', 'Gender', 'Gen Pos']:
+					participant[header] = element
+				elif header in ['Time', 'Swim', 'Cycle', 'Run']:
+					participant[header] = convert_time_to_seconds(element)
+			participants.append(participant)
 
-	participants = []
+	return participants
+	
+def output_csv(participants):
+	"""Make a csv file with the processed data"""
+	with open('processed_data.csv', 'w+') as processed:
+		for i,key in enumerate(participants[0]):
+			processed.write(key)
+			if i < (len(participants[0]) - 1):
+				processed.write(',')
+		processed.write('\n')
+		for participant in participants:
+			for i,key in enumerate(participant):
+				processed.write(str(participant[key]))
+				if i < (len(participant) - 1):
+					processed.write(',')
+			processed.write('\n')
 
-	for line in lines[1:]:
-		participant = {}
-		for i,element in enumerate(line.split(',')):
-			header = headers[i]
-			if header in ['Pos','Category', 'Cat Pos', 'Age', 'Gender', 'Gen Pos']:
-				participant[header] = element
-			elif header in ['Time', 'Swim', 'Bike', 'Run']:
-				participant[header] = convert_time_to_seconds(element)
-		participants.append(participant)
+def main():
+	participants = parse_triathlon_data('olympic_tri_results.csv')
+	output_csv(participants)
 
-	for participant in participants:
-		print participant
+if __name__ == '__main__':
+	main()
